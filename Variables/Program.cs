0cf8e7a7ee5e4ad434d9ba94731e98ba29e20d3a
OldBeliver <380506338004@yandex.ua>
2022-03-11 15:43:50 +0200

    Добавьте файлы проекта.

---
+++ Variables/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string login = "qwerty";
+            string password = "login";
+            
+            ShowInfo(login, password);
+            InverseData(ref login, ref password);
+            ShowInfo(login, password);
+        }
+
+        static void ShowInfo(string login, string password)
+        {
+            Console.WriteLine($"login: {login}\npassword: {password}");
+        }
+
+        static void InverseData(ref string login, ref string password)
+        {
+            string template = login;
+            login = password;
+            password = template;
+        }
+    }
+}
